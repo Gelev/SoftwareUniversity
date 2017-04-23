9f55d863d54b4b78d996ebee211ae1fe3aa6a1b1
Gelev <dragomir.gelev@gmail.com>
2017-04-23 20:11:30 +0300

    JS Objects - lab

---
+++ JavaScript Fundamentals/JS Objects, JSON, Maps and Sets - lab/01.Towns to JSON.js
@@ -0,0 +1,15 @@
+function townsToJSON(input) {
+    let regEx = /\s*\|+\s*/g;
+    let outputTitles = input.shift().split(regEx).filter(t => t != '');
+    let towns = [];
+    for (let townData of input) {
+        townData = townData.split(regEx).filter(t => t != '');
+        let town = {};
+        town[outputTitles[0]] = townData[0];
+        town[outputTitles[1]] = Number(townData[1]);
+        town[outputTitles[2]] = Number(townData[2]);
+        towns.push(town);
+    }
+
+    return JSON.stringify(towns);
+}
