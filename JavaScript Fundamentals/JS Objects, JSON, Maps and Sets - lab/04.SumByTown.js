a842d98e6f0f426f166b4c53537c8e21ab411c5f
Gelev <dragomir.gelev@gmail.com>
2017-05-11 19:28:15 +0300

    JS Objects Lab

---
+++ JavaScript Fundamentals/JS Objects, JSON, Maps and Sets - lab/04.SumByTown.js
@@ -0,0 +1,13 @@
+function sumByTown(data){
+    let sums = {};
+    for(let i = 0; i < data.length; i +=2){
+        let town = data[i];
+        let income = Number(data[i + 1]);
+
+        if(!sums[town]){
+            sums[town] = 0;
+        }
+        sums[town] += income;
+    }
+    console.log(sums);
+}
